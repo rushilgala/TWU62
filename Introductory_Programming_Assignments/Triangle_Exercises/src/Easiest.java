0712215d71d20c08d929fa3fbbb377e3c8d1c85f
Rushil Gala-Shah <rushil@gala-shah.co.uk>
2018-06-29 23:21:58 +0100

    Printing one asterisk to console

--- Introductory_Programming_Assignments/Triangle_Exercises/src/Easiest.java
+++ Introductory_Programming_Assignments/Triangle_Exercises/src/Easiest.java
@@ -1,5 +1,5 @@
 public class Easiest {
-    public static void main(String[] args) {
-        System.out.println("*");
-    }
+  public static void main(String[] args) {
+    System.out.println("*");
+  }
 }
